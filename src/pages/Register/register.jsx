667ea45cfb7b4f320df48fa5c591c5b55aa2c547
AyushPanday1 <ayush7426883261@gmail.com>
2023-06-15 16:48:35 +0530

    Done

--- src/pages/Register/register.jsx
+++ src/pages/Register/register.jsx
@@ -34,103 +34,35 @@ function Resister() {
         if (!validateEmail(email)) { return alert("Please enter valid Email !") }
 
 
-        const config = {
-            headers: {
-                'x-content-type-options': 'multipart/form-data'
-            },
-        };
+        alert("Account has created succesfully")
+        window.location.replace("/")
 
-
-        const options = {
-            name: name,
-            email: email,
-            phone: phone,
-            password: password,
-
-        }
-        axios.post("http://localhost:8080/register", options, config).then((res) => {
-            alert("Account has created succesfully")
-            window.location.replace("/")
-        }).catch((err) => alert(err.message))
     }
 
-   
+
 
 
     return (
-        // <div className='main-div1'>
-        //     <div className="container h-100">
 
-        //         <div className="d-flex justify-content-center h-100">
-
-        //             <div className="user_card1">
-
-        //                 <div className="d-flex justify-content-center">
-
-        //                     <div className="d-flex justify-content-center form_container1">
-        //                         <form onSubmit={resisterUser}>
-
-        //                           <h2>Sign Up</h2>
-
-        //                             <div className="resister-form">
-
-        //                                 <input type="text" className='inputs' placeholder='Username' required value={name} onChange={(e) => setName(e.target.value)} />
-        //                             </div>
-        //                             <div className="resister-form">
-
-        //                                 <input type="email" className='inputs' placeholder='Email' required value={email} onChange={(e) => setEmail(e.target.value)} />
-        //                             </div>
-        //                             <div className="resister-form">
-
-        //                                 <input type="number" className='inputs' placeholder='Contact' required value={phone} onChange={(e) => setPhone(e.target.value)} />
-        //                             </div>
-
-
-        //                             <div className="resister-form">
-
-        //                                 <input className='inputs' type="text" placeholder='City' value={city} onChange={(e) => setCity(e.target.value)} />
-        //                             </div>
-        //                             <div className="resister-form">
-
-        //                                 <input className='inputs' type="text" placeholder='Country' value={street} onChange={(e) => setStreet(e.target.value)} />
-        //                             </div>
-
-
-        //                             <div className="resister-form">
-
-        //                                 <input type="password" className='inputs' placeholder='Password' required value={password} onChange={(e) => setPassword(e.target.value)} />
-        //                             </div>
-
-
-
-        //                             <button type="submit" id='btn' className="btn btn-primary">Create Account</button>
-        //                             <div className='d-flex justify-content-center links ele'>Already have an account?</div><a id='logins' href='/'>Login</a>
-        //                         </form>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
         <div className="formContainer">
-        <div className="formWrapper">
-          <span className="logo">WanderStay</span>
-          <span className="title">Sign Up
-          </span>
-          <form onSubmit={resisterUser}>
-            <input  type="text" placeholder="Username" required value={name} onChange={(e) => setName(e.target.value)}/>
-            <input  type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input  type="Contact" placeholder="Contact" required value={phone} onChange={(e) => setPhone(e.target.value)}/>
-            
-            <input  type="password" placeholder="password"  onChange={(e) => setPassword(e.target.value)} />
-            <button type="submit">Sign up</button>
-            
-          </form>
-          <p>
-            You do have an account? <a id='logins' href='/'>Login</a>
-          </p>
+            <div className="formWrapper">
+                <span className="logo">WanderStay</span>
+                <span className="title">Sign Up
+                </span>
+                <form onSubmit={resisterUser}>
+                    <input type="text" placeholder="Username" required value={name} onChange={(e) => setName(e.target.value)} />
+                    <input type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="Contact" placeholder="Contact" required value={phone} onChange={(e) => setPhone(e.target.value)} />
+
+                    <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit">Sign up</button>
+
+                </form>
+                <p>
+                    You do have an account? <a id='logins' href='/'>Login</a>
+                </p>
+            </div>
         </div>
-      </div>
     )
 }
 
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import {  useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 import "./login.css";
 
@@ -7,43 +6,20 @@ const Login = () => {
 
 const navigate = useNavigate()
 
-const [email, setEmail] = useState("")
-const [password, setPassword] = useState("")
-
 
 	const loginFunction = (e) => {
-		e.preventDefault()
-
-		let obj = {
-			email: email,
-			password: password
-		}
-
-		axios.post("http://localhost:8080/login", obj).then((res) => {
-			localStorage.setItem("user", JSON.stringify(res.data.data))
+		
 			
       navigate('/Home')
+      alert("Logged in succesfully")
 
-		}).catch((err) => { alert(err.response.data.message) })
 	}
 
 
 
 
   return (
-    // <div className="login">
-    //   <div className="lContainer">
-    //   <input type="text"  placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-    //   <input type="password"  placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-    //     <button  onClick={loginFunction} className="lButton">
-    //       Login
-    //     </button>
-      
-    //     <p>
-    //         Don't have an account? <a id='register' href='/register'>Sign Up</a>
-    //       </p>
-    //   </div>
-    // </div>
+   
     <div className="formContainer1">
     <div className="formWrapper1">
       <span className="logo">WanderStay</span>
